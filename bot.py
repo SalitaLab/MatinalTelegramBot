e9c23890cd5b7e0e356092d33f26f8693ed6f7d8
tomyahu <tomyahu@gmail.com>
2018-04-29 19:01:40 -0300

    first commit

---
+++ bot.py
@@ -0,0 +1,47 @@
+from telegram.ext import Updater, CommandHandler
+import logging
+import hidden
+from model.telegram_queue import Telegram_Queue
+
+telQueue = Telegram_Queue()
+telQueue.load_from_file("database/telegram_messages.txt")
+
+#Funcion de start
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Hola soy un bot :D")
+
+def setPun(bot, update):
+    texto = update.message.text.split(' ', 1)[1]
+    pun_file = open("database/pun.txt","w")
+    pun_file.write(texto)
+    pun_file.close()
+    bot.send_message(chat_id=update.message.chat_id, text="La nueva pun es:\n" + "\"" + texto + "\"")
+
+def comment(bot, update):
+    telQueue.push(update.message.text.split(' ', 1)[1])
+
+def save(bot, update):
+    telQueue.save_in_file("database/telegram_messages.txt")
+    bot.send_message(chat_id=update.message.chat_id, text="¡Los comentarios se han guardado!")
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+updater = Updater(hidden.token)
+dispatcher = updater.dispatcher
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+set_pun_handler = CommandHandler('setPun', setPun)
+dispatcher.add_handler(set_pun_handler)
+
+comment_handler = CommandHandler('c', comment)
+dispatcher.add_handler(comment_handler)
+
+save_handler = CommandHandler('save', save)
+dispatcher.add_handler(save_handler)
+
+updater.start_polling()
+updater.idle()
---
+++ model/telegram_queue.py
@@ -0,0 +1,48 @@
+
+class Queue():
+
+    def __init__(self):
+        self.queue = []
+
+    def length(self):
+        return len(self.queue)
+
+    def empty(self):
+        return len(self.queue) == 0
+
+    def push(self, object):
+        self.queue.append(object)
+
+    def pop(self):
+        if not self.empty():
+            result = self.queue[0]
+            self.queue = self.queue[1:]
+            return result
+
+
+class Telegram_Queue(Queue):
+
+    def __init__(self):
+        Queue.__init__(self)
+
+    def save_in_file(self, path):
+        texto = ""
+        for i in range(len(self.queue)):
+            texto += self.queue[i]
+            texto += "\n"
+
+        file = open(path, "w")
+        file.write(texto)
+        file.close()
+
+    def load_from_file(self, path):
+        file = open(path, "r")
+        array = file.read().split('\n')
+        array = array[:(len(array) - 1)]
+
+        self.queue = array
+
+        file.close()
+
+    def clear(self):
+        self.queue = []
