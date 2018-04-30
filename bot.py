1124b067178b100d9abb9abfdac88dd1e364f224
tomyahu <tomyahu@gmail.com>
2018-04-30 03:06:34 -0300

    Added Aesthetics

--- bot.py
+++ bot.py
@@ -14,20 +14,19 @@ def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Hola soy un bot :D")
 
 def setPun(bot, update):
-    texto = update.message.text.split(' ', 1)[1]
-    pun_file = open("database/pun.txt","w")
-    pun_file.write(texto)
-    pun_file.close()
-    bot.send_message(chat_id=update.message.chat_id, text="La nueva pun es:\n" + "\"" + texto + "\"")
+    array = update.message.text.split(' ', 1)
+    if(len(array) > 1):
+        texto = array[1]
+        pun_file = open("database/pun.txt","w")
+        pun_file.write(texto)
+        pun_file.close()
+        bot.send_message(chat_id=update.message.chat_id, text="La nueva pun es:\n" + "\"" + texto + "\"")
 
 def comment(bot, update):
-    telQueue.push(update.effective_user.username + ": " + update.message.text.split(' ', 1)[1])
-    telQueue.save_in_file("database/telegram_messages.txt")
-
-def save(bot, update):
-    telQueue.save_in_file("database/telegram_messages.txt")
-    bot.send_message(chat_id=update.message.chat_id, text="¡Los comentarios se han guardado!")
-
+    texto = update.message.text.split(' ', 1)
+    if (len(texto) > 1):
+        telQueue.push(update.effective_user.username + ": " + texto[1])
+        telQueue.save_in_file("database/telegram_messages.txt")
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -43,9 +42,6 @@ dispatcher.add_handler(set_pun_handler)
 
 comment_handler = CommandHandler('c', comment)
 dispatcher.add_handler(comment_handler)
-
-save_handler = CommandHandler('save', save)
-dispatcher.add_handler(save_handler)
 
 updater.start_polling()
 updater.idle()
